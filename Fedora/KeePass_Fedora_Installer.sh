4cbbc864a07bcd05f9db9752dc6c3f3947c1db03
Jonathan Jensen <haiyun211@gmail.com>
2014-04-19 09:08:40 -0600

    Upadate script to download KeePass and add .gitignore file

--- Fedora/KeePass_Fedora_Installer.sh
+++ Fedora/KeePass_Fedora_Installer.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-yum -y install mono-core
-yum -y install mono-devel
-mkdir /usr/bin/KeePass
 
-if rpm -qa | grep -q wget; then
-	wget http://sourceforge.net/projects/keepass/files/KeePass%202.x/2.26/KeePass-2.26.zip/download?accel_key=77%3A1397887777%3Ahttp%253A//keepass.info/download.html%3Ae49b83c7%2442662cbc0291be4b61413dd22bae180bc244eae5&click_id=2f7f50ca-c789-11e3-88c2-0200ac1d1d9c-3&source=accel 
-else
+if ! rpm -qa | grep -q wget; then
 	yum -y install wget
 fi
+wget "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.26/KeePass-2.26.zip?r=&ts=1397896195&use_mirror=tcpdiag" -O KeePass-2.26.zip
+wget "http://upload.wikimedia.org/wikipedia/commons/1/19/KeePass_icon.png" -O KeePass_icon.png
+wget "https://github.com/jonje/Scripts/blob/master/resources/KeePass.desktop" -O KeePass.desktop
+wait
+mkdir /usr/bin/KeePass
 unzip KeePass-2.26.zip -d /usr/bin/KeePass/
-wget http://upload.wikimedia.org/wikipedia/commons/1/19/KeePass_icon.png
+
 mv KeePass_icon.png /usr/bin/KeePass/KeePass_icon.png
-wget https://github.com/jonje/Scripts/blob/master/resources/KeePass.desktop
+
 mv KeePass.desktop /usr/share/applications/
+yum -y install mono-core
+yum -y install mono-devel
+

72d0c8b23254befda892d11157e424a4b16288c6
Jonathan Jensen <haiyun211@gmail.com>
2014-04-19 21:58:19 -0600

    Works completely

--- Fedora/KeePass_Fedora_Installer.sh
+++ Fedora/KeePass_Fedora_Installer.sh
@@ -5,7 +5,7 @@ if ! rpm -qa | grep -q wget; then
 fi
 wget "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.26/KeePass-2.26.zip?r=&ts=1397896195&use_mirror=tcpdiag" -O KeePass-2.26.zip
 wget "http://upload.wikimedia.org/wikipedia/commons/1/19/KeePass_icon.png" -O KeePass_icon.png
-wget "https://github.com/jonje/Scripts/blob/master/resources/KeePass.desktop" -O KeePass.desktop
+wget "https://raw.githubusercontent.com/jonje/Scripts/master/resources/KeePass.desktop" -O KeePass.desktop 
 wait
 mkdir /usr/bin/KeePass
 unzip KeePass-2.26.zip -d /usr/bin/KeePass/
